9073ea7753b137764d4fc22a4a9103a1ae65fa8e
manuel <manuel@manuels-MacBook-Pro.local>
2018-02-24 13:57:43 -0500

    added firebase

--- WSIE/App.js
+++ WSIE/App.js
@@ -1,6 +1,17 @@
 'use strict';
 import React, { Component } from 'react';
 import { StackNav } from './config/router';
+import * as firebase from 'firebase';
+
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+  const config = {
+    apiKey: "apiKey",
+    authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://databaseName.firebaseio.com",
+    storageBucket: "bucket.appspot.com"
+  };
+  firebase.initializeApp(config);
 
 export default class App extends Component {
   render () {
